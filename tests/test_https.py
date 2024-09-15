774ef1397bffee776a3d017d076ca33cadd67707
kk <112804222+kar-ik@users.noreply.github.com>
2024-09-15 22:01:54 +0530

    Update test_https.py

--- tests/test_https.py
+++ tests/test_https.py
@@ -1,1 +1,11 @@
+from framework import send_request, log_to_report
+
+def test_https(url):
+    if not url.startswith("https://"):
+        log_to_report(f"[-] {url} is not using HTTPS")
+        print(f"[-] {url} is not using HTTPS")
+    else:
+        log_to_report(f"[+] {url} is using HTTPS")
+        print(f"[+] {url} is using HTTPS")
+
 
